2dda392603d49b19f9f0854176b481df487c9901
Donghun Won <wonhun1225@gmail.com>
2025-11-03 22:29:33 +0900

    feat : 유저 repository

--- src/main/java/ita/growin/GrowinApplication.java
+++ src/main/java/ita/growin/GrowinApplication.java
@@ -2,8 +2,10 @@ package ita.growin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GrowinApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/ita/growin/domain/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package ita.growin.domain.user.repository;
+
+import ita.growin.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByKakaoId(Long kakaoId);
+    Optional<User> findByEmail(String email);
+    boolean existsByNickname(String nickname);
+}
+
