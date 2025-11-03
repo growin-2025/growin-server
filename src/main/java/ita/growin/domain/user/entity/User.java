e4e2745d2b7bb4f6c2014fb978a0912b0926d218
Donghun Won <wonhun1225@gmail.com>
2025-11-03 22:26:14 +0900

    feat : 엔티티 생성

--- src/main/java/ita/growin/domain/user/entity/User.java
+++ src/main/java/ita/growin/domain/user/entity/User.java
@@ -1,26 +1,50 @@
 package ita.growin.domain.user.entity;
 
-
 import ita.growin.domain.event.entity.Event;
+import ita.growin.domain.user.constant.*;
+import ita.growin.global.entity.BaseEntity;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.Comment;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "users")
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 @AllArgsConstructor
-public class User {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
-    private Long id;
+    @GeneratedValue
+    @Comment("uid")
+    private Long userId;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private LoginType type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus status;
+
+    @Column(nullable = false, length = 100)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private Work work;
+
+    @Enumerated(EnumType.STRING)
+    private InterestField interestField;
+
+    @Enumerated(EnumType.STRING)
+    private Target target;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Event> events = new ArrayList<>();
